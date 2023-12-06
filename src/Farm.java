cdcf69d386f72e0b5303576b4f47f4ae4a622297
Jwhite5013 <152316827+Jwhite5013@users.noreply.github.com>
2023-12-06 16:42:58 +0000

    Added Farm and started main

--- src/Farm.java
+++ src/Farm.java
@@ -1,14 +1,18 @@
 public class Farm{
-private Acre[][]Land;
-  public Farm(int rows, int col){
-Acre[][]temp = new Acre[rows][col];
-Land = temp;
+private Acre[][] Land;
+public Farm(int r, int c){
+
   }
 public void plant(String plan, int row, int col){
-Land[row][col] = plan;
+
 }
 public String toString(){
-  
+  for(int i = 0; i<Land.length; i++){
+    for(int j = 0; j<Land[i].length;j++){
+  String temp = Land[i][j].toString();
+      return temp;
+    }
+  }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+  // ArrayList<Farm> land = new ArrayList<Farm>();
+      Scanner scan = new Scanner(System.in);
+      System.out.println("How many rows is your farm?");
+    int rows = scan.nextInt();
+      scan.nextLine();
+     System.out.println("How many columns are there?");
+      int col = scan.nextInt();
+     // land.add() 
     }
 }
